5ac28e94d75b85e030688088845445a3a1300adb
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-05-04 16:53:06 +0600

    fetch data

--- src/app/allBooks/page.jsx
+++ src/app/allBooks/page.jsx
@@ -1,17 +1,27 @@
+import BookCard from "@/components/BookCard";
 
 
+const AllBooks = async () => {
 
-const AllBooks = () => {
+    const res = await fetch('https://biko-book.vercel.app/data.json');
+    const books = await res.json();
 
-
+    console.log(books);
 
 
 
     return (
         <div>
+            <h2 className="text-2xl font-bold text-center my-5 text-white bg-[#830039] rounded-2xl p-3">All Books</h2>
 
 
-            allBooks
+            <div className="mx-auto grid w-7xl lg:grid-cols-4 gap-14">
+                {
+                    books.map(book => <BookCard key={book.id} book={book}></BookCard>)
+                }
+            </div>
+
+
 
         </div>
     );
--- src/components/BooksApi.jsx
+++ src/components/BooksApi.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import BookCard from './BookCard';
 
+const BooksApi = async () => {
+    const res = await fetch('https://biko-book.vercel.app/data.json')
+    const data = await res.json()
 
-const BooksApi = () => {
+    const topBooks = data.slice(0, 4)
 
 
 
     return (
         <div className='w-11/12 mx-auto'>
+            <h1 className='text-4xl font-bold mt-20 mb-5 text-center'>Recommended For You</h1>
+            <div className='border border-b-2 mb-5 border-purple-100 w-6xl mx-auto'></div>
 
-            Api page
+            <div className='grid grid-cols-4 gap-10 max-w-6xl  mx-auto'>
+                {
+                    topBooks.map(book =>
+                        <BookCard key={book.id} book={book}></BookCard>
+                    )
+                }
+            </div>
+
         </div>
     );
 };
